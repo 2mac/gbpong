e57df2af9c70f74cad7d1c9fc74e0db90fa7136a
Chad Perrin <code@apotheon.net>
2021-08-09 14:48:13 -0700

    upgrade license to COIL-0.6

--- src/gamemode.h
+++ src/gamemode.h
@@ -2,17 +2,16 @@
  *  gbpong - Pong on the Game Boy
  *  Copyright (C) 2016-2017 David McMackins II
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, version 3 only.
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the Copyfree Open Innovation License, version 0.6 only.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ *  Copyfree Open Innovation License for more details.
  *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the Copyfree Open Innovation License
+ *  along with this program.  If not, see <http://coil.apotheon.org>.
  */
 
 #ifndef GBPONG_GAMEMODE_H
